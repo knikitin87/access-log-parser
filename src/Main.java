1878215fe04cc51955a963e8861d4469ff59a46f
Konstantin Nikitin <k.nikitin987@gmail.com>
2025-06-25 14:16:00 +0300

    fixed calculator

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = scanner.nextLine();
-        int length = text.length();
-        System.out.println("Длина текста: " + length);
-        scanner.close();
+        System.out.println("Введите первое число:");
+       int firstNumber =  new Scanner(System.in) .nextInt();
+
+        System.out.println("Введите второе число:");
+        int secondNumber =  new Scanner(System.in) .nextInt();
+
+        int x = firstNumber + secondNumber;
+        int y = firstNumber - secondNumber;
+        int z = firstNumber * secondNumber;
+        double q = (double) firstNumber / secondNumber;
+
+        System.out.println("Сумма: " + x);
+        System.out.println("Разность: " + y);
+        System.out.println("Произведение: " + z);
+        System.out.println("Частное: " + q);
+
+
     }
 }
+
